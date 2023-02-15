71b279fbda716bc335230940f6268aae600bdb43
nstarlike <nstar0512@gmail.com>
2023-02-15 15:16:39 +0900

    add springboot and maven files

---
+++ src/main/java/nstarlike/usermicroservice/UsermicroserviceApplication.java
@@ -0,0 +1,13 @@
+package nstarlike.usermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermicroserviceApplication.class, args);
+	}
+
+}
