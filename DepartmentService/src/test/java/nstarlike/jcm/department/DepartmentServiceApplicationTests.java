60bfb9f6d202559313e102dfa4b449db2d9eec4d
nstarlike <nstar0512@gmail.com>
2023-04-19 18:45:39 +0900

    Add Department Service project skeleton

---
+++ DepartmentService/src/test/java/nstarlike/jcm/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nstarlike.jcm.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
