91e16c1c3e928b2888d7fd2f744e657ae8515dc7
nstarlike <nstar0512@gmail.com>
2023-04-26 03:02:44 +0900

    Mark as a service

--- EmailService/src/main/java/nstarlike/jcm/email/service/impl/EmailServiceImpl.java
+++ EmailService/src/main/java/nstarlike/jcm/email/service/impl/EmailServiceImpl.java
@@ -3,9 +3,11 @@ package nstarlike.jcm.email.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import nstarlike.jcm.email.service.EmailService;
 
+@Service
 public class EmailServiceImpl implements EmailService {
 	@Autowired
 	private JavaMailSender javaMailSender;
